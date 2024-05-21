4bd11e48f98a9eb4cbfbccee6181cc118a4a4f08
Simone Ruggeri <simone.ruggeri3@studio.unibo.it>
2024-05-21 18:47:11 +0200

    Created homepage for client

---
+++ public/homepage-cliente.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../template/header/header.css">
+  <link rel="stylesheet" href="styles/homepage-cliente.css">
+  <title>HomePage</title>
+</head>
+
+<body>
+  <?php include "../template/header/header.html";?>
+  <main>
+    <div class="left-side-container">
+      <div class="info-container">
+        <img src="../assets/icons/person.svg" alt="person-logo">
+        <p>ID CLIENTE</p>
+        <p>Nome e Cognome</p>
+        <p>Email</p>
+      </div>
+    </div>
+    <div class="right-side-container">
+      <div class="btn-container">
+        <button>Cerca Nutrizionista</button>
+        <button>Il tuo Nutrizionista</button>
+        <button>Storico Nutrizionisti</button>
+        <button>Prescrizioni</button>
+        <button>Diario</button>
+      </div>
+    </div>
+  </main>
+</body>
+
+</html>
