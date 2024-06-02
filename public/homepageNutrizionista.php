7d551912d818a3c44f1376d798d4eb9870f0d0eb
Simone Ruggeri <simone.ruggeri3@studio.unibo.it>
2024-06-02 15:20:16 +0200

    Added info profile to nutritionist's homepage

--- public/homepageNutrizionista.php
+++ public/homepageNutrizionista.php
@@ -11,39 +11,64 @@
 
 <body>
   <?php include "../template/header/header.html";
-  require ("../bootstrap.php");
+  require("../bootstrap.php");
   $id = $_SESSION["id"];
-  require ("../nutrizionista.php"); ?>
+  require("../nutrizionista.php"); ?>
   <main>
     <div class="left-side-container">
       <div class="info-container">
         <img src="../assets/icons/person.svg" alt="person-logo">
         <p>ID NUTRIZIONISTA: <?php echo $_SESSION["id"]; ?></p>
-        <p class="nome">
+        <p class="nome bold">
           <?php echo $templateparams["nutrizionista"][0]["Nome"], " ", $templateparams["nutrizionista"][0]["Cognome"]; ?>
-        </p>
-        <p class="email"><?php echo $templateparams["nutrizionista"][0]["Email"]; ?></p>
-        <label for="valutazione">Valutazione</label>
+        </p>        
+        <div class="info">
+          <p class="bold">Qualifica</p>
+          <div class="info-dettagli">
+            <p><?php echo $templateparams["nutrizionista"][0]["Titolo"]; ?></p>
+          </div>
+        </div>
+        <div class="info">
+          <p class="bold">Informazioni</p>
+          <div class="info-dettagli">
+            <p>Esperienza: <?php echo $templateparams["nutrizionista"][0]["Esperienza"]; ?></p>
+            <p>Specializzazione: <?php echo $templateparams["nutrizionista"][0]["Specializzazione"]; ?></p>
+            <p>Orari: <?php echo $templateparams["nutrizionista"][0]["Orari"]; ?></p>
+            <p class="email">Email: <?php echo $templateparams["nutrizionista"][0]["Email"]; ?></p>
+            <p>Telefono: <?php echo $templateparams["nutrizionista"][0]["Telefono"]; ?></p>
+          </div>
+        </div>
+        <div class="indirizzo">
+          <p class="bold">Indirizzo</p>
+          <div class="indirizzo-info">
+            <p><?php echo $templateparams["nutrizionista"][0]["Via"]; ?></p>
+            <p><?php echo $templateparams["nutrizionista"][0]["NCivico"]; ?>,</p>
+            <p><?php echo $templateparams["nutrizionista"][0]["Citta"]; ?>,</p>
+            <p><?php echo $templateparams["nutrizionista"][0]["CAP"]; ?></p>
+          </div>
+        </div>
+        <label class="bold" for="valutazione">Valutazione</label>
         <?php
         $mediaInt = $templateparams["nutrizionista"][0]["MediaVoti"];
         if ($mediaInt == 0) {
           echo "Questo nutrizionista non ha ancora ricevuto recensioni.";
-      } else {
-        for ($i = 0; $i < 5; $i++):
-          if ($i < $mediaInt):
-            ?>
-            <img src="../assets/icons/star.svg" alt="star">
-            <?php
-          endif;
-          if ($i >= $valutazioneInt):
-            ?>
-            <img src="../assets/icons/star.svg" alt="">
-            <?php
-          endif;
-        endfor;
-      }
+        } else {
         ?>
-        <button onclick="window.location.href='recensioni.php'">Vedi Recensioni</button>
+          <div class="rating">
+            <?php
+            for ($i = 0; $i < 5; $i++) {
+              if ($i < $mediaInt) {
+            ?>
+                <img src="../assets/icons/star.svg" alt="star">
+            <?php
+              }
+            }
+            ?>
+          </div>
+          <button onclick="window.location.href='recensioni.php'">Vedi Recensioni</button>
+        <?php
+        }
+        ?>
       </div>
     </div>
     <div class="right-side-container">
