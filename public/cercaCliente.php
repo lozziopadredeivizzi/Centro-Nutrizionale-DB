666d9310df5af384d623ea43af42a721ceabf661
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-06-22 18:15:39 +0200

    added check for nutrizionista's clients

--- public/cercaCliente.php
+++ public/cercaCliente.php
@@ -37,25 +37,29 @@
       <br>
       <br>
       <?php echo $_GET["text"];
-    } else {
+    } else if (!empty($templateparams["ultimaScelta"])) {
       foreach ($templateparams["ultimaScelta"] as $scelta):
         if ($scelta["E_L_ultima_scelta"] == "Sì"):
           $cliente = $dbh->getClienteTable()->getClienteById($scelta["IDCliente"]); ?>
-          <div onclick="window.location.href='profiloCliente.php'" class="result"
-            data-id="<?php echo $cliente[0]["IDCliente"]; ?>">
-            <ul>
-              <li>ID: <?php echo $cliente[0]["IDCliente"]; ?></li>
-              <li><?php echo $cliente[0]["Nome"], " ", $cliente[0]["Cognome"]; ?></li>
-              <li>Città: <?php echo $cliente[0]["Citta"]; ?></li>
-            </ul>
-          </div>
+            <div onclick="window.location.href='profiloCliente.php'" class="result"
+              data-id="<?php echo $cliente[0]["IDCliente"]; ?>">
+              <ul>
+                <li>ID: <?php echo $cliente[0]["IDCliente"]; ?></li>
+                <li><?php echo $cliente[0]["Nome"], " ", $cliente[0]["Cognome"]; ?></li>
+                <li>Città: <?php echo $cliente[0]["Citta"]; ?></li>
+              </ul>
+            </div>
         <?php endif;
       endforeach;
-    } ?>
-  </main>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="../js/exit.js" type="text/javascript"></script>
-  <script src="../js/viewProfileCliente.js" type="text/javascript"></script>
+    } else { ?>
+        <br>
+        <br>
+        <?php echo "Non hai nessun cliente al momento." ?>
+    <?php } ?>
+    </main>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="../js/exit.js" type="text/javascript"></script>
+    <script src="../js/viewProfileCliente.js" type="text/javascript"></script>
 </body>
 
 </html>
